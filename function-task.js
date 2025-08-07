177a45b7c537859fa1976c4675647b6adb5fe659
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-08 01:02:06 +0600

    js-function-task problime solving

---
+++ function-task.js
@@ -0,0 +1,12 @@
+//Take four parameters. Multiply the four numbers and then return the result
+function multiplyNumbers(a, b, c, d) {
+  let result = a * b * c * d;
+//   console.log(result);
+  return result;
+}
+
+console.log("output" + " " + multiplyNumbers(2, 3, 4, 5));
+
+// another way 
+let output = multiplyNumbers(2, 3, 4, 5);
+console.log("output" + " " + output); 
