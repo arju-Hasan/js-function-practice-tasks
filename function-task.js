3206070bcaf9b2ad72498eae87727fb1b39e34d9
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-08 01:17:53 +0600

    add a task 2

--- function-task.js
+++ function-task.js
@@ -10,3 +10,16 @@ console.log("output" + " " + multiplyNumbers(2, 3, 4, 5));
 // another way 
 let output = multiplyNumbers(2, 3, 4, 5);
 console.log("output" + " " + output); 
+
+
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function oddOrEven(num){
+    if(num % 2 == 0){
+         console.log(num * 2);
+    }
+    else console.log(num * 3);
+       
+}
+oddOrEven(9)
