af05ba5b3b0f4a64c17758a307f67b2b0d6aa501
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-08 02:18:18 +0600

    add a task number 5 problam solving

--- function-task-solving.js
+++ function-task-solving.js
@@ -57,3 +57,18 @@ function count_zero(input){
 }
 let counts = count_zero("010100");
 console.log(counts)
+
+
+// Task-5
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+
+function odd_even(num){
+    if ( num % 2 === 0 ){
+        console.log("Odd Number")
+    }
+    else{
+        console.log("even Number")
+    }
+}
+odd_even(13)
